ab23fe9687dffc314159d6c64a889db83f6ca739
Seulgeant <leo_04_10@hotmail.com>
2022-05-02 23:01:35 -0300

    tp clientes terminados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ClientesModule } from './clientes/clientes.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ClientesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clientes/cliente.model.ts
@@ -0,0 +1,18 @@
+
+export interface Cliente {
+    id: number;
+    nombre: string;
+    apellido: string;
+    dni: string;
+    direccion:string;
+    cif:string;
+    correo: string;
+    grupo: number;
+   }
+   export interface Grupo {
+    id: number;
+    nombre: string;
+    
+   }
+  
+   
